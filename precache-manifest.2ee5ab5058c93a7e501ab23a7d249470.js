4808c483106ea9756d8a8c2ccc87d1e8ca412d6b
Millie W <mw3235@nyu.edu>
2019-11-15 16:10:23 -0800

    Updates

---
+++ precache-manifest.2ee5ab5058c93a7e501ab23a7d249470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58a526a6d374771c4540e0dea867a0a",
+    "url": "/hot-dog-101/index.html"
+  },
+  {
+    "revision": "4ce7d5b6222160365941",
+    "url": "/hot-dog-101/static/css/main.c3f546b2.chunk.css"
+  },
+  {
+    "revision": "87664c3a84d49d6e08c3",
+    "url": "/hot-dog-101/static/js/2.86aa6dca.chunk.js"
+  },
+  {
+    "revision": "4ce7d5b6222160365941",
+    "url": "/hot-dog-101/static/js/main.e74b010c.chunk.js"
+  },
+  {
+    "revision": "9eb9037f29b5be7b1729",
+    "url": "/hot-dog-101/static/js/runtime-main.f122b1bb.js"
+  },
+  {
+    "revision": "1edf7a32c3a1471f34d74234762e8880",
+    "url": "/hot-dog-101/static/media/arrow-1.1edf7a32.svg"
+  },
+  {
+    "revision": "16edcd3160384fa7186472bdf05a9d1c",
+    "url": "/hot-dog-101/static/media/logo.16edcd31.png"
+  }
+]);
